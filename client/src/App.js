e533be461205dac1f0e3727fbf2c7b9a46e37fb3
Purvi Shah <purvi@streamlending.com.au>
2019-02-20 11:26:20 +1100

    Indicate game draw if no one wins

--- client/src/App.js
+++ client/src/App.js
@@ -92,6 +92,8 @@ class Game extends React.Component {
         let status;
         if (winner) {
             status = 'Winner: ' + winner;
+        } else if (this.state.stepNumber === 9) {
+            status= 'Game Drawn';
         } else {
             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
